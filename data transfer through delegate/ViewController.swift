1b4e32894e563b8ef39b0752f3da25062906c3f6
Deekonda Venkatesh <deekondavenkatesh01@achiever-46.local>
2019-11-18 11:14:41 +0530

    Initial Commit

---
+++ data transfer through delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  data transfer through delegate
+//
+//  Created by Deekonda Venkatesh on 11/18/19.
+//  Copyright © 2019 Deekonda Venkatesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ data transfer through delegateTests/data_transfer_through_delegateTests.swift
@@ -0,0 +1,34 @@
+//
+//  data_transfer_through_delegateTests.swift
+//  data transfer through delegateTests
+//
+//  Created by Deekonda Venkatesh on 11/18/19.
+//  Copyright © 2019 Deekonda Venkatesh. All rights reserved.
+//
+
+import XCTest
+@testable import data_transfer_through_delegate
+
+class data_transfer_through_delegateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
